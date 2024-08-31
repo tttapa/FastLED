c11ee7e061156105ca40e82e16ace28db5dcbfa1
zackees <z@zackees.com>
2024-08-31 12:38:35 -0700

    adds led strip to compile sources

--- dev/dev.ino
+++ dev/dev.ino
@@ -2,33 +2,24 @@
 /// @brief   Blink the first LED of an LED strip
 /// @example Blink.ino
 
+#include <stdio.h>
+//#include "freertos/FreeRTOS.h"
+//#include "freertos/task.h"
+//#include "led_strip.h"
+//#include "esp_log.h"
+//#include "esp_err.h"
+
+
+
 #include <FastLED.h>
 
-// How many leds in your strip?
-#define NUM_LEDS 10
+#include "platforms/esp/32/led_strip/led_strip.h"
 
-// For led chips like WS2812, which have a data line, ground, and power, you just
-// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
-// ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
-// Clock pin only needed for SPI based chipsets when not using hardware SPI
-#define DATA_PIN 9
+// #include "platforms/esp/32/led_strip/internal_namespace.h"
 
-// Define the array of leds
-CRGB leds[NUM_LEDS];
+// using namespace fastled_esp_internal;
 
-void setup() {
-    Serial.begin(115200);
-    FastLED.addLeds<WS2812, DATA_PIN>(leds, NUM_LEDS);  // GRB ordering is assume
-    delay(2000);  // If something ever goes wrong this delay will allow upload.
-}
 
-void loop() { 
-  // Fill the LED array with red, then show
-  fill_solid(leds, NUM_LEDS, CRGB::Red);
-  FastLED.show();
-  delay(500);
-  // Fill the LED array with black (off), then show
-  fill_solid(leds, NUM_LEDS, CRGB::Black);
-  FastLED.show();
-  delay(500);
-}
+
+void setup() {}
+void loop() {}
